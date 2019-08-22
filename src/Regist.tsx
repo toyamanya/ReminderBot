01f4427c647be6b79b7a629c609a42563eb2a240
toyamanya <ryotoyamaiii@gmail.com>
2019-08-23 00:59:10 +0900

    hook ver

--- src/Regist.tsx
+++ src/Regist.tsx
@@ -1,25 +1,10 @@
-import React, { Component, useState } from "react";
+import React, { useState, FunctionComponent } from "react";
 import Button from "@material-ui/core/Button";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import { styled } from "@material-ui/styles";
+import { string } from "prop-types";
 
-
-interface Props {
-  nowTime: Date;
-}
-interface State {
-  eventName: string;
-  content: string;
-  eventDay: string; // 型が怪しい
-  eventTime: string; // 型が怪しい
-  eventPlace: string;
-  postDay: string;
-  postTime: string;
-  channel: string; // 型が怪しい
-  channels: string[];
-  remindTime: string; // 型が怪しい
-}
 
 // styledの一つ目の括弧はコンポーネント、二つ目の括弧はスタイル
 const MyButton = styled(({ color, ...other }) => <Button {...other} />)({
@@ -51,50 +36,40 @@ const MyTextField = styled(TextField)({
   width: 400
 });
 
+export default function Regist<>() {
+  const [eventName, setEventName] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [eventDayAndTime, seteventDayAndTime] = useState<string>("");
+  const [place, setPlace] = useState<string>("");
+  const [postDayAndTime, setpostDayAndTime] = useState<string>("");
+  const [channel, setchannel] = useState<string>("");
+  const [channels, setchannels] = useState<string[]>(["general", "ramdom"]);
 
-export default class Regist extends Component<Props, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      eventName: "",
-      content: "",
-      eventDay: "",
-      eventTime: "",
-      eventPlace: "",
-      postDay: "",
-      postTime: "",
-      channel: "",
-      channels: ["general", "random"],
-      remindTime: "",
-    };
-  }
+  const channelList = channels.map((ch, i) => {
+    return (
+      <MenuItem key={i} value={ch}>
+        {ch}
+      </MenuItem>
+    );
+  });
 
-  onClickRoot = () => {
+  const onClickRoot = () => {
     console.log("一覧ページに移動します");
   };
 
-  onclickRegist = () => {
+  const onclickRegist = () => {
     console.log("登録ページに移動します");
-  };
 
-  render() {
-    const channelList = this.state.channels.map((ch, i) => {
-      return (
-        <MenuItem key={i} value={ch}>
-          {ch}
-        </MenuItem>
-      );
-    });
 
     return (
       <CenterDiv>
         <div>
-          <MyButton color="red" onClick={this.onClickRoot}>
+          <MyButton color="red" onClick={onClickRoot}>
             一覧ページ
           </MyButton>
-          <MyButton color="blue" onClick={this.onclickRegist}>
+          <MyButton color="blue" onClick={onclickRegist}>
             登録ページ
-          </MyButton>
+        </MyButton>
         </div>
 
         <h1>リマインダー登録</h1>
@@ -106,17 +81,18 @@ export default class Regist extends Component<Props, State> {
                 required
                 id="eventName"
                 label="イベント名(必須)"
-                value={this.state.eventName}
-                onChange={e => this.setState({ eventName: e.target.value })}
+                value={eventName}
+                onChange={e => setEventName(e.target.value)}
                 margin="normal"
               />
             </div>
+
             <div>
               <MyTextField
                 id="config"
                 label="説明文"
-                value={this.state.content}
-                onChange={e => this.setState({ content: e.target.value })}
+                value={content}
+                onChange={e => setContent(e.target.value)}
                 multiline
                 margin="normal"
               />
@@ -126,62 +102,48 @@ export default class Regist extends Component<Props, State> {
               <MyTextField
                 id="day"
                 label="イベントの日付"
-                value={this.state.eventDay}
-                onChange={e => this.setState({ eventDay: e.target.value })}
+                value={eventDayAndTime}
+                onChange={e => seteventDayAndTime(e.target.value)}
                 margin="normal"
               />
             </div>
-            <div>
-              <MyTextField
-                id="time"
-                label="イベントの時間"
-                value={this.state.eventTime}
-                onChange={e => this.setState({ eventTime: e.target.value })}
-                margin="normal"
-              />
-            </div>
+
             <div>
               <MyTextField
                 id="place"
                 label="イベントの場所"
-                value={this.state.eventPlace}
-                onChange={e => this.setState({ eventPlace: e.target.value })}
+                value={place}
+                onChange={e => setPlace(e.target.value)}
                 margin="normal"
               />
             </div>
+
             <div>
               <MyTextField
                 id="postDay"
                 label="投稿する日付"
-                value={this.state.postDay}
-                onChange={e => this.setState({ postDay: e.target.value })}
+                value={postDayAndTime}
+                onChange={e => setpostDayAndTime(e.target.value)}
                 margin="normal"
               />
             </div>
-            <div>
-              <MyTextField
-                id="postTime"
-                label="投稿する時間"
-                value={this.state.postTime}
-                onChange={e => this.setState({ postTime: e.target.value })}
-                margin="normal"
-              />
-            </div>
+
             <div>
               <MyTextField
                 id="channel"
                 select
                 label="チャンネル"
-                value={this.state.channel}
-                onChange={e => this.setState({ channel: e.target.value })}
+                value={channel}
+                onChange={e => setchannel(e.target.value)}
                 margin="normal"
               >
                 {channelList}
               </MyTextField>
             </div>
+
           </form>
         </div>
       </CenterDiv>
-    );
+    )
   }
 }
